bfddc5847366cba2df6d6f7f16ff3161b4e2bc29
sundaymark <sundayukpai1122@gmail.com>
2022-10-13 23:02:34 +0100

    added the accordion function

---
+++ script.js
@@ -0,0 +1,8 @@
+const accordion = document.getElementsByClassName('container-content');
+for(i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active');
+
+    })
+
+}
