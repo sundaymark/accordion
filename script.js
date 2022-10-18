efaf07bd280bafcfa0aace63b18dcd97bf618816
sundaymark <sundayukpai1122@gmail.com>
2022-10-18 20:14:40 +0100

    added the dom

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const accordion = document.getElementsByClassName('container-content');
-for(i = 0; i < accordion.length; i++){
-    accordion[i].addEventListener('click',function(){
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
         this.classList.toggle('active');
 
     })
